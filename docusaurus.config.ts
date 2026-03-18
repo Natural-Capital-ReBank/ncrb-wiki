43eac721a11dff98accf1464c5c8daf44bfb154a
zalam003 <admin@baip.ai>
2026-03-18 18:08:10 -0400

    fix link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -131,7 +131,7 @@ const config: Config = {
           items: [
             {
               label: 'Welcome To NCRB',
-              to: '/docs/about',
+              to: '/docs/intro',
             },
           ],
         },
